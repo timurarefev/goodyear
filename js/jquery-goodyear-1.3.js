8128e62a313cf4ca985108bdb5b80d35399cfb79
timurarefev <t2002mor@ya.ru>
2015-06-10 18:07:43 +0600

    И еще разок

--- js/jquery-goodyear-1.3.js
+++ js/jquery-goodyear-1.3.js
@@ -2876,7 +2876,7 @@
     			Формат, переданный в data
     		*/
             
-        	if (typeof(goodyear_input.data("goodyearNoIcon")) != "undefined" && goodyear_input.data("noIcon") == true)
+        	if (typeof(goodyear_input.data("goodyearNoIcon")) != "undefined" && goodyear_input.data("goodyearNoIcon") == true)
         	{
         		options.no_icon = true;
         	} else
